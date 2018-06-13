f5aa5772f3ea17ed9fd66d53e6f1d0aae1ef1303
Andy0414 <pjh8667@naver.com>
2018-06-13 20:12:31 +0900

    side

--- js.js
+++ js.js
@@ -1,20 +1,25 @@
 var topBar = document.getElementById('topBar');
+var aside = document.getElementById('ad')
 addEventListener('scroll', () => {
     if (scrollY > 0) {
         topBar.style.top = '0px';
         topBar.style.opacity = '1';
+        
     }
     else {
         topBar.style.opacity = '0';
+        
     }
     var userH = window.innerHeight * 0.9
     if (scrollY > userH) {
         topBar.style.height = '100px';
         topBar.style.boxShadow = '0px 0px 40px #416275';
+        aside.style.opacity = "0.8";
     }
     else {
         topBar.style.boxShadow = '0px 0px 0px #416275';
         topBar.style.height = (window.innerHeight - scrollY) + 'px';
+        aside.style.opacity = "0";
     }
 })
 
